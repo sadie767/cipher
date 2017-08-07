b66043655e98c5862381d156992046ca873cf4b7
Rebecca McIntosh and senya webster <rebecca@>
2017-08-07 16:31:25 -0700

    add final function before bonus

--- scripts.js
+++ scripts.js
@@ -32,30 +32,57 @@
 
 // <<------3rd function------>>>>
 
-var sentence = prompt("enter a sentence dammit");
+// var sentence = prompt("enter a sentence dammit");
+//
+// console.log(sentence);
+//
+// var firstlast = function(sentence) {
+//   var first = sentence.charAt(0).toUpperCase();
+//   var lastchar = sentence.length-1;
+//   var last = sentence.charAt(lastchar).toUpperCase();
+//
+//   return first + last;
+// }
+// var end = firstlast(sentence);
+// console.log(end);
+//
+// var firstlast = function(sentence) {
+//   var first = sentence.charAt(0).toUpperCase();
+//   var lastchar = sentence.length-1;
+//   var last = sentence.charAt(lastchar).toUpperCase();
+//
+//   return last + first;
+// }
+// var end = firstlast(sentence);
+// console.log(end);
+//
 
-console.log(sentence);
-
-var firstlast = function(sentence) {
-  var first = sentence.charAt(0).toUpperCase();
-  var lastchar = sentence.length-1;
-  var last = sentence.charAt(lastchar).toUpperCase();
-
-  return first + last;
-}
-var end = firstlast(sentence);
-console.log(end);
-
-var firstlast = function(sentence) {
-  var first = sentence.charAt(0).toUpperCase();
-  var lastchar = sentence.length-1;
-  var last = sentence.charAt(lastchar).toUpperCase();
-
-  return last + first;
-}
-var end = firstlast(sentence);
-console.log(end);
-
+// <<------4th function------>>>>
+//
+// var sentence = prompt("enter a sentence dammit");
+//
+// console.log(sentence);
+//
+// var firstlast = function(sentence) {
+//   var first = sentence.charAt(0).toUpperCase();
+//   var lastchar = sentence.length-1;
+//   var last = sentence.charAt(lastchar).toUpperCase();
+//
+//   return first + last;
+// }
+// var end = firstlast(sentence);
+// console.log(end);
+//
+// var firstlast = function(sentence) {
+//   var first = sentence.charAt(0).toUpperCase();
+//   var lastchar = sentence.length-1;
+//   var last = sentence.charAt(lastchar).toUpperCase();
+//
+//   return sentence + last + first;
+// }
+// var end = firstlast(sentence);
+// console.log(end);
+//
 
 // <<------4th function------>>>>
 
@@ -77,8 +104,11 @@ var firstlast = function(sentence) {
   var first = sentence.charAt(0).toUpperCase();
   var lastchar = sentence.length-1;
   var last = sentence.charAt(lastchar).toUpperCase();
+  var total = sentence.length;
+  var target = Math.floor((total)/2);
+  var letter = sentence.charAt(target);
 
-  return last + first;
+  return letter + sentence + last + first;
 }
 var end = firstlast(sentence);
 console.log(end);
