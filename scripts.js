4a5806b6d2b053461ffb4ba85f0f88aadb32b481
Rebecca McIntosh and senya webster <senya@>
2017-08-07 15:52:24 -0700

    add fourth function

---
+++ scripts.js
@@ -0,0 +1,84 @@
+//
+//
+//
+// var sentence = prompt("enter a sentence dammit");
+//
+// console.log(sentence);
+//
+// var firstlast = function(sentence) {
+//   var first = sentence.charAt(0).toUpperCase();
+//   var lastchar = sentence.length-1;
+//   var last = sentence.charAt(lastchar).toUpperCase();
+//
+//   return first + last;
+// }
+// var end = firstlast(sentence);
+// console.log(end);
+//
+//
+// var sentence = prompt("enter a sentence dammit");
+//
+// console.log(sentence);
+//
+// var firstlast = function(sentence) {
+//   var first = sentence.charAt(0).toUpperCase();
+//   var lastchar = sentence.length-1;
+//   var last = sentence.charAt(lastchar).toUpperCase();
+//
+//   return last + first;
+// }
+// var end = firstlast(sentence);
+// console.log(end);
+
+// <<------3rd function------>>>>
+
+var sentence = prompt("enter a sentence dammit");
+
+console.log(sentence);
+
+var firstlast = function(sentence) {
+  var first = sentence.charAt(0).toUpperCase();
+  var lastchar = sentence.length-1;
+  var last = sentence.charAt(lastchar).toUpperCase();
+
+  return first + last;
+}
+var end = firstlast(sentence);
+console.log(end);
+
+var firstlast = function(sentence) {
+  var first = sentence.charAt(0).toUpperCase();
+  var lastchar = sentence.length-1;
+  var last = sentence.charAt(lastchar).toUpperCase();
+
+  return last + first;
+}
+var end = firstlast(sentence);
+console.log(end);
+
+
+// <<------4th function------>>>>
+
+var sentence = prompt("enter a sentence dammit");
+
+console.log(sentence);
+
+var firstlast = function(sentence) {
+  var first = sentence.charAt(0).toUpperCase();
+  var lastchar = sentence.length-1;
+  var last = sentence.charAt(lastchar).toUpperCase();
+
+  return first + last;
+}
+var end = firstlast(sentence);
+console.log(end);
+
+var firstlast = function(sentence) {
+  var first = sentence.charAt(0).toUpperCase();
+  var lastchar = sentence.length-1;
+  var last = sentence.charAt(lastchar).toUpperCase();
+
+  return last + first;
+}
+var end = firstlast(sentence);
+console.log(end);
